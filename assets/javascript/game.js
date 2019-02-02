d434812d15530002a36965a6c2e6e3392e1d4390
Ho-Jung Tseng (Rachael) <45810198+hojungt@users.noreply.github.com>
2019-02-01 23:19:55 -0500

    game.js var and logical operator edit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,23 +3,52 @@ var characterNames = ["mickey", "ariel", "simba", "aladin",];
 var alphabets = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
 "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-// Variables to hold the number of wins and loses
-var guessedRight = 0;
-var guessedWrong = 0;
+// Variables - hold the number of wins and loses
+var guessedRightAnswer = [];
+var guessedRight = [];
+var guessedWrong = [];
 var chanceRemaining = 10;
 
-// Variables that hold references to HTML
+// Variables - hold references to HTML
 var directionsText = document.getElementById("directions-text");
 var guessedRight = document.getElementById("guessed-right-text");
 var guessedWrong = document.getElementById("guessed-wrong-text");
 var chanceRemaining = document.getElementById("chance-remaining");
 
-// Functions - generate random name from array
-var Name = Math.floor(Math.random()*characterNames.length);
+// Variables - generate random name from array
+var answerName = characterNames[Math.floor(Math.random() * characterNames.length)];
+var answerNameSplit = answerName.split("");
+
+
+var answerNameSplitCharacter
 
 // Functions - event listener on user guess
 document.onkeyup = function(event) {
+
+    // vvv does not work
+    // directionsText.textContent = "";
+
     var userGuess = event.key;
+
+    for (i = 0; i < answerName.length; i++) {
+        if (answerName.charAt(i) == upperKeyGuess) {
+            if (rightGuess.indexOf(upperKeyGuess) < 0) {
+                rightGuess.push(upperKeyGuess);
+            }
+        }
+
+    if (userGuess === answerName.charAt(i) && userGuess === alphabets.charAt(i)) {
+        guessedRight++;
+    }
+    else if (userGuess !== answerName.charAt(i) && userGuess === alphabets.charAt(i)) {
+        guessedWrong++;
+        chanceRemaining--;
+
+    }
+    else {
+
+    }
+
 }
 
 
@@ -27,5 +56,5 @@ document.onkeyup = function(event) {
 // chartAt  logical operators   answer box and wrong alphabets?
 
 
-// Hide the directions
-directionsText.textContent = "";
+
+// // Hide the directions
