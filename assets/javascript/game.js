9f7889a6bf151196c15e8c00b4c2cd3549cf1a95
Ho-Jung Tseng (Rachael) <45810198+hojungt@users.noreply.github.com>
2019-02-02 00:23:22 -0500

    logical operator edit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,60 +1,58 @@
 // Arrays that lists out all of the options
-var characterNames = ["mickey", "ariel", "simba", "aladin",];
+var characterNames = ["minnie", "ariel", "elsa", "jasmine", "belle"];
 var alphabets = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
 "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
+// Variables - generate random name from array
+var answerName = characterNames[Math.floor(Math.random() * characterNames.length)];
+var answerNameSplit = answerName.split(",");
+console.log (answerNameSplit);
+
 // Variables - hold the number of wins and loses
-var guessedRightAnswer = [];
+var answerSpaces = [];
 var guessedRight = [];
 var guessedWrong = [];
 var chanceRemaining = 10;
 
 // Variables - hold references to HTML
 var directionsText = document.getElementById("directions-text");
+var answerSpaces = document.getElementById("answer-spaces-text");
 var guessedRight = document.getElementById("guessed-right-text");
 var guessedWrong = document.getElementById("guessed-wrong-text");
 var chanceRemaining = document.getElementById("chance-remaining");
 
-// Variables - generate random name from array
-var answerName = characterNames[Math.floor(Math.random() * characterNames.length)];
-var answerNameSplit = answerName.split("");
-
-
-var answerNameSplitCharacter
+// Function - create answer spaces???
+function showAnswerSpaces() {
+    for (i = 0; i < answerNameSplit.length; i++) {
+    answerSpaces.push("_");
+    }
+}
 
 // Functions - event listener on user guess
 document.onkeyup = function(event) {
 
-    // vvv does not work
     // directionsText.textContent = "";
 
     var userGuess = event.key;
+    // var userGuessUppercase = userGuess.toUpperCase();
+    // var alphabetsUppercase = alphabets.toUpperCase();
 
-    for (i = 0; i < answerName.length; i++) {
-        if (answerName.charAt(i) == upperKeyGuess) {
-            if (rightGuess.indexOf(upperKeyGuess) < 0) {
-                rightGuess.push(upperKeyGuess);
-            }
-        }
-
-    if (userGuess === answerName.charAt(i) && userGuess === alphabets.charAt(i)) {
-        guessedRight++;
-    }
-    else if (userGuess !== answerName.charAt(i) && userGuess === alphabets.charAt(i)) {
-        guessedWrong++;
-        chanceRemaining--;
-
-    }
-    else {
-
-    }
+    // for (i = 0; i < alphabets.length; i++) {
+    //     if (userGuess === alphabets[i]) {
+    //         for (j = 0; j < characterNames.length; j++){
+    //             if (userGuess === answerNameSplit.charAt(j)) {
+    //                 guessedRight.push("");
+    //             }
+    //             else {
+    //                 guessedWrong.push("");
+    //             }
+    //         }
+    //     }
+    // }
 
 }
 
 
-// check all variables are declared and connected to HTML
-// chartAt  logical operators   answer box and wrong alphabets?
-
-
-
-// // Hide the directions
+// chartAt?
+// logical operators?
+// Hide the directions?
