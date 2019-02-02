161a604f630aff5426b44878bb067af264ee1d3b
Ho-Jung Tseng (Rachael) <45810198+hojungt@users.noreply.github.com>
2019-02-02 12:58:30 -0500

    fix underscored answer space game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,15 +5,16 @@ var alphabets = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 
 // Variables - generate random name from array
 var answerName = characterNames[Math.floor(Math.random() * characterNames.length)];
-var answerNameSplit = answerName.split(",");
+
+var answerNameSplit = answerName.split("");
 console.log (answerNameSplit);
 
 // Variables - hold the number of wins and loses
-var answerSpaces = [];
+var answerSpaces;
 var guessedRight = [];
 var guessedWrong = [];
 var chanceRemaining = 10;
-
+console.log(answerSpaces);
 // Variables - hold references to HTML
 var directionsText = document.getElementById("directions-text");
 var answerSpaces = document.getElementById("answer-spaces-text");
@@ -23,10 +24,13 @@ var chanceRemaining = document.getElementById("chance-remaining");
 
 // Function - create answer spaces???
 function showAnswerSpaces() {
+    answerSpaces = [];
     for (i = 0; i < answerNameSplit.length; i++) {
-    answerSpaces.push("_");
+        console.log(i);
+        answerSpaces.push("_");
     }
 }
+showAnswerSpaces();
 
 // Functions - event listener on user guess
 document.onkeyup = function(event) {
@@ -49,6 +53,9 @@ document.onkeyup = function(event) {
     //         }
     //     }
     // }
+    // console.log(userGuess);
+    answerNameSplit.includes(userGuess);
+    // console.log(answerNameSplit.includes(userGuess));
 
 }
 
