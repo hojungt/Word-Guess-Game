786789e2eaeb45b124509c217c2ea9864a5a06c3
Ho-Jung Tseng (Rachael) <45810198+hojungt@users.noreply.github.com>
2019-02-02 19:01:57 -0500

    logical operator edit for guessedWrong. checking logical operator edit for guessedRight

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,32 +5,31 @@ var alphabets = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 
 // Variables - generate random name from array
 var answerName = characterNames[Math.floor(Math.random() * characterNames.length)];
-
 var answerNameSplit = answerName.split("");
-console.log (answerNameSplit);
+console.log(answerName);
+console.log(answerNameSplit);
 
-// Variables - hold the number of wins and loses
-var answerSpaces;
+// Variables - hold the record of wins and loses
+var answerSpaces = [];
 var guessedRight = [];
 var guessedWrong = [];
 var chanceRemaining = 10;
-console.log(answerSpaces);
-// Variables - hold references to HTML
-var directionsText = document.getElementById("directions-text");
-var answerSpaces = document.getElementById("answer-spaces-text");
-var guessedRight = document.getElementById("guessed-right-text");
-var guessedWrong = document.getElementById("guessed-wrong-text");
-var chanceRemaining = document.getElementById("chance-remaining");
 
-// Function - create answer spaces???
+// Variables - provide references to HTML
+var answerSpacesText = document.getElementById("answer-spaces-text");
+var guessedRightText = document.getElementById("guessed-right-text");
+var guessedWrongText = document.getElementById("guessed-wrong-text");
+var chanceRemaining = document.getElementById("chance-remaining");
+var directionsText = document.getElementById("directions-text");
+
+// Function - create answer spaces
 function showAnswerSpaces() {
-    answerSpaces = [];
     for (i = 0; i < answerNameSplit.length; i++) {
         console.log(i);
         answerSpaces.push("_");
     }
 }
-showAnswerSpaces();
+showAnswerSpaces() 
 
 // Functions - event listener on user guess
 document.onkeyup = function(event) {
@@ -38,27 +37,38 @@ document.onkeyup = function(event) {
     // directionsText.textContent = "";
 
     var userGuess = event.key;
+    console.log (userGuess);
     // var userGuessUppercase = userGuess.toUpperCase();
     // var alphabetsUppercase = alphabets.toUpperCase();
 
-    // for (i = 0; i < alphabets.length; i++) {
-    //     if (userGuess === alphabets[i]) {
-    //         for (j = 0; j < characterNames.length; j++){
-    //             if (userGuess === answerNameSplit.charAt(j)) {
-    //                 guessedRight.push("");
-    //             }
-    //             else {
-    //                 guessedWrong.push("");
-    //             }
-    //         }
-    //     }
-    // }
-    // console.log(userGuess);
-    answerNameSplit.includes(userGuess);
-    // console.log(answerNameSplit.includes(userGuess));
+    if (answerNameSplit.includes(userGuess)) {
+        var index = answerNameSplit.indexOf(userGuess);
+        if (index !== -1) {
+            answerSpaces[index] = userGuess;
+        }
+        guessedRight.push(userGuess);
+        guessedRight.sort();
+    }
+    else {
+        guessedWrong.push(userGuess);
+        guessedWrong.sort();
 
+        function chanceDeduct(num1) {
+            num1 - 1;
+        }
+        return chanceDeduct();
+    }    
+
+    console.log (answerSpaces);
+    console.log (guessedRight);
+    console.log (guessedWrong);
 }
 
+// Function - write HTML content 
+// function write() {
+//     document.getElementById("answer-space-text").innerHTML = answerSpacesText.join("");
+// }
+// console.log(write(answerSpacesText));
 
 // chartAt?
 // logical operators?
